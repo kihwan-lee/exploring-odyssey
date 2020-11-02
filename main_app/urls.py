65073a66024c68ef8f4f0100ab35dadc4c333004
j-blalock <justin.m.blalock@gmail.com>
2020-11-02 17:44:18 -0600

    got login redirecting correctly

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views 
+from django.contrib import admin
 from django.conf.urls.static import static
 from .models import City, Author, Article
 
