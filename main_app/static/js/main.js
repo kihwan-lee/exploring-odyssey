0a1e24df59832260ce74fd6975bc561ab49142b8
andyvdoan <andyy.doan@gmail.com>
2020-11-05 23:40:33 -0600

    Added sample sql query to add location table and added styling for various pages

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -7,3 +7,27 @@ function showNav() {
   document.getElementsByClassName("homeNav")[0].classList.toggle("active");
 }
 
+// MDB Validating Search Bar (https://mdbootstrap.com/docs/jquery/forms/select/)
+$(document).ready(function() {
+  $('.mdb-select.validate').materialSelect({
+    validate: true,
+    labels: {
+      validFeedback: "Let's search!",
+      invalidFeedback: "Sorry, we don't have that one yet.."
+    }
+  });
+function validateSelect(e) {
+    e.preventDefault();
+    $('.needs-validation').addClass('was-validated');
+    if ($('.needs-validation select').val() === null) {
+      $('.needs-validation').find('.valid-feedback').hide();
+      $('.needs-validation').find('.invalid-feedback').show();
+      $('.needs-validation').find('.select-dropdown').val('').prop('placeholder', 'No countries selected')
+    } else {
+      $('.needs-validation').find('.valid-feedback').show();
+      $('.needs-validation').find('.invalid-feedback').hide();
+    }
+  }
+  $('.needs-validation select').on('change', e => validateSelect(e))
+  $('.needs-validation').on('submit', e => validateSelect(e))
+});
