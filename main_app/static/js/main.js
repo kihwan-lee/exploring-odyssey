d6ba1ea6b18e1d45c83ce3589047a5412706d883
andyvdoan <andyy.doan@gmail.com>
2020-11-06 16:57:37 -0600

    Article detail page now renders random images based on city

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -2,35 +2,10 @@
 //      Modal Nav Menu      //
 //==========================//
 
-// Toggle Menu
+// Toggle Nav Menu
 function showNav() {
   document.getElementsByClassName("homeNav")[0].classList.toggle("active");
 }
-
-// MDB Validating Search Bar (https://mdbootstrap.com/docs/jquery/forms/select/)
-$(document).ready(function() {
-  $('.mdb-select.validate').materialSelect({
-    validate: true,
-    labels: {
-      validFeedback: "Let's search!",
-      invalidFeedback: "Sorry, we don't have that one yet.."
-    }
-  });
-function validateSelect(e) {
-    e.preventDefault();
-    $('.needs-validation').addClass('was-validated');
-    if ($('.needs-validation select').val() === null) {
-      $('.needs-validation').find('.valid-feedback').hide();
-      $('.needs-validation').find('.invalid-feedback').show();
-      $('.needs-validation').find('.select-dropdown').val('').prop('placeholder', 'No countries selected')
-    } else {
-      $('.needs-validation').find('.valid-feedback').show();
-      $('.needs-validation').find('.invalid-feedback').hide();
-    }
-  }
-  $('.needs-validation select').on('change', e => validateSelect(e))
-  $('.needs-validation').on('submit', e => validateSelect(e))
-});
 
 // Ajax call to signal views.py for city page
 // var url = $( '#selection-form' ).attr( 'action' );
